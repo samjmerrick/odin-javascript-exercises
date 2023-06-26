18e26b7184c7f2d79244c0170630f90cc7bf97d6
Sam Merrick <samjmerrick@gmail.com>
2023-06-26 13:06:10 +0100

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+  // 	x °F ≘ (x − 32) × 5/9°C
+  const result = (farenheit - 32) * (5 / 9);
+  return round(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  // x °C ≘ (x × 9/5+ 32) °F
+  const result = celsius * (9 / 5) + 32;
+  return round(result);
+};
+
+const round = function (number) {
+  return Math.round(number * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
