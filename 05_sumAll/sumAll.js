a102dfe99f8d20ac04df63b6f5a0ec5deb95bf07
Sam Merrick <samjmerrick@gmail.com>
2023-06-26 11:22:46 +0100

    05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (typeof start !== "number" || typeof end !== "number") return "ERROR";
+  if (start < 0 || end < 0) return "ERROR";
 
+  if (start > end) {
+    const tempStart = start;
+    start = end;
+    end = tempStart;
+  }
+
+  let num = 0;
+  for (i = start; i <= end; i++) {
+    num += i;
+  }
+  return num;
 };
 
 // Do not edit below this line
