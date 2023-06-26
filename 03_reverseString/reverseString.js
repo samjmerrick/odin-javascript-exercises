879e8f8d3a61d9412efea534eb7e0fececfd0885
Sam Merrick <samjmerrick@gmail.com>
2023-06-26 11:02:08 +0100

    03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const array = string.split("");
+  const reverse = array.reverse();
+  return reverse.join("");
 };
 
 // Do not edit below this line
