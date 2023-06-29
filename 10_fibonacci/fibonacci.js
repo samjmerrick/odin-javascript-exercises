4daa402433158c4515d83ed7087560dd1a76b1d4
Sam Merrick <samjmerrick@gmail.com>
2023-06-29 15:13:55 +0100

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS"; 
+    let fib = [0, 1];
+    // generate fibonnachi
+    for(let i = 1; i < num; i++) {
+        fib.push(fib[i] + fib[i -1]);
+    };
+    return fib[num];
 };
 
 // Do not edit below this line
