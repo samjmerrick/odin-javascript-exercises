15d90853727fbfd53a6decf657138560cc7b63c5
Sam Merrick <samjmerrick@gmail.com>
2023-06-29 14:59:33 +0100

    09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const lower = str.toLowerCase();
+    const arr = lower.match(/[a-z0-9]/g);
+    const length = arr.length;
 
+    // Loop until the middle, check for match at opposite position
+    for (let i = 0; i < (length / 2); i++) {
+        if(arr[i] != arr[length - 1 - i]) { 
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
