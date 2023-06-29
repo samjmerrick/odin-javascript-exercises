e52212648960c59a3e115b654f317e3c1a8ebe2c
Sam Merrick <samjmerrick@gmail.com>
2023-06-29 14:39:04 +0100

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((t, v) => t += v, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  return args.reduce((t, v) => t *= v, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let sum = 1;
+  for (let i = num; i > 0; i--){
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
