eac62985f239e72a74f1887dc7226b31b4e136f2
Sam Merrick <samjmerrick@gmail.com>
2023-06-29 15:45:28 +0100

    12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
+const year = new Date().getFullYear();
 
+const findTheOldest = function(arr) {
+    return arr.reduce((prev, curr) => calculateAge(curr) > calculateAge(prev) ? curr : prev);
 };
+
+function calculateAge(person) {
+    return ((person.yearOfDeath ?? year) - person.yearOfBirth);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
